fe10ff7041e938eac8d68e50f9bd292d1ea64606
Morcaen <rolandcameron179@gmail.com>
2025-10-19 19:07:14 +1300

    Initial commit

---
+++ Assets/Skeleton.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Skeleton
+{
+    # Store the joints and bones of the skeleton of a given organism
+    public Skeleton(int numJoints, int numBones, float flexibility, float boneLen)
+    {
+
+    }
+}
